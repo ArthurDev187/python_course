d9e83699fbcd08dbfe04c1f82fcbe94c5358c863
Silvanis <silvanis.silva@outlook.com>
2023-08-08 20:45:08 -0300

    fdsa

---
+++ aula55.py
@@ -0,0 +1,10 @@
+"""
+Exiba os indices e itens da lista
+"""
+
+contador = 0
+lista = ['Arthur', 'Nani', 'Maria', 'Paula', 'Stefani', 'Ana']
+
+for i in lista:
+    print(f'indice: {contador}   item: {i} ')
+    contador += 1
