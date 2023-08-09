f1241757079442e5ef9f960f216e72af32b0d130
Arthur Rodrigues <arthurr.rodrigues187@gmail.com>
2023-08-09 15:26:51 -0300

    Adicionando o testo da próxima atividade

--- aula59.py
+++ aula59.py
@@ -1,1 +1,4 @@
-#teste
+"""
+Faça um programa que seja uma lista de compras. O usuário deve ter a possibilidade de inserir, 
+apagar e listar valores da sua lista. Não permita que o programa quebre com erros de indices inexistentes na lista.
+"""
