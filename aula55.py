687859af1e0290b10890e86d39fbb489f6fad84a
Silvanis <silvanis.silva@outlook.com>
2023-08-08 21:06:34 -0300

    fdaas

--- aula55.py
+++ aula55.py
@@ -2,9 +2,14 @@
 Exiba os indices e itens da lista
 """
 
-contador = 0
-lista = ['Arthur', 'Nani', 'Maria', 'Paula', 'Stefani', 'Ana']
 
-for i in lista:
-    print(f'indice: {contador}   item: {i} ')
-    contador += 1
+lista = ['Arthur', 'Nani', 'Maria', 'Paula', 'Stefani', 'Ana']
+# contador = 0
+# for i in lista:
+#     print(f'indice: {contador}   item: {i} ')
+#     contador += 1
+
+indice = range(len(lista))
+
+for i in indice:
+    print(i, lista[i])
