7121ee3e2dc3124063c6931950019c311d1deb4c
Arthur Rodrigues <arthurr.rodrigues187@gmail.com>
2023-09-05 20:27:18 -0300

    Learning something else here

--- aula110.py
+++ aula110.py
@@ -2,16 +2,23 @@
 
 
 try:
+    # print('Linha1'[1000])
     a = 12
     b = 2
     c = a / b
     print(c)
-except ZeroDivisionError:
-    print('Erro divisão por zero.')
-except NameError:
-    print('variável a ou b não definidas.')
-except TypeError:
-    print('Type Error')
+except ZeroDivisionError as e:
+    print(e.__class__.__name__)
+    print(e)
+except NameError as e:
+    print(e.__class__.__name__)
+    print(e)
+except IndexError as e:
+    print(e.__class__.__name__)
+    print(e)
+except TypeError as e:
+    print(e.__class__.__name__)
+    print(e)
 
 
     
